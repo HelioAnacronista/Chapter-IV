5cf55f73f12895816f9e6ef4c48b8c33357ca5be
Hélio Fernandes <104803451+HelioAnacronista@users.noreply.github.com>
2023-01-23 19:05:35 -0300

    feat: Sinalizando link ativo

---
+++ src/components/ActiveLink.tsx
@@ -0,0 +1,41 @@
+import Link, { LinkProps } from "next/link";
+import { useRouter } from "next/router";
+import { ReactElement, cloneElement } from "react";
+
+interface ActiveLinkProps extends LinkProps {
+  children: ReactElement;
+  //
+  shouldMatchExactHref?: boolean;
+}
+
+export function ActiveLink({
+  children,
+  //verifica se rota começa com o ativo
+  shouldMatchExactHref = false,
+  ...rest
+}: ActiveLinkProps) {
+  //pega a rota ativa
+  const { asPath } = useRouter();
+
+  let isActive = false;
+
+  if (shouldMatchExactHref && (asPath === rest.href || asPath === rest.as)) {
+    isActive = true;
+  }
+
+  if (
+    !shouldMatchExactHref &&
+    (asPath.startsWith(String(rest.href)) || asPath.startsWith(String(rest.as)))
+  ) {
+    isActive = true;
+  }
+
+  //clona o primerio elemento que vem no componente.
+  return (
+    <Link {...rest}>
+      {cloneElement(children, {
+        color: isActive ? "pink.400" : "gray.50",
+      })}
+    </Link>
+  );
+}
